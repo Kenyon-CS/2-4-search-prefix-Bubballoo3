b2ce5a726933eee40838333d93d99c9a4aee7f69
Kenyon User <kenyonuser@math-k120358.lan>
2024-02-04 13:00:41 -0500

    Assignment Submission

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,5 +1,7 @@
 def searchPrefix(arr,s)
-
+    plength=s.length
+    selected=arr.select {|value| value[0...plength]==s}
+    return selected
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
